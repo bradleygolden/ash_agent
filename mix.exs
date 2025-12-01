82eb93d61020db8d0f80b1f21da4db4ee2bcc5b2
Bradley Golden <golden.bradley@gmail.com>
2025-11-30 22:19:47 -0600

    Add mix_audit dependency for hex.audit support

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule AshAgent.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
       {:plug, "~> 1.16", only: :test}
     ] ++ ash_baml_dep()
   end
